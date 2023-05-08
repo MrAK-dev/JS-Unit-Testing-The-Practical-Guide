baa02a782c757dd14b272bf1795d700b8410772f
Alex <alex.fiendish@gmail.com>
2023-05-08 20:06:14 +0300

    added code examples of tests in output.test.js

---
+++ src/parser.js
@@ -0,0 +1,13 @@
+export function extractNumbers(formData) {
+  const num1Input = formData.get('num1');
+  const num2Input = formData.get('num2');
+
+  return [num1Input, num2Input];
+}
+
+export function extractEnteredNumberValues(form) {
+  const formData = new FormData(form);
+  const numberInputs = extractNumbers(formData);
+
+  return numberInputs;
+}
