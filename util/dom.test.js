68f0f355c3cf5e650c53512e87d45e82e473bdd5
Alex <alex.fiendish@gmail.com>
2023-05-09 18:56:56 +0300

    added code examples of tests which interact with DOM

--- util/dom.test.js
+++ util/dom.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-import { vi, it } from 'vitest';
+import { vi, it, expect, beforeEach } from 'vitest';
 import { Window } from 'happy-dom';
 
 import { showError } from './dom';
@@ -13,9 +13,36 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-document.write(htmlDocumentContent);
 vi.stubGlobal('document', document);
 
-it('first test', () => {
-  showError('test');
+beforeEach(() => {
+  document.body.innerHTML = '';
+  document.write(htmlDocumentContent);
+});
+
+it('should add an error paragraph to the id="errors" element', () => {
+  showError('Test');
+
+  const errorsEl = document.getElementById('errors');
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).not.toBeNull();
+});
+
+it('should not contain an error paragraph initially', () => {
+  const errorsEl = document.getElementById('errors');
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).toBeNull();
+});
+
+it('should output the provided message in the error paragraph', () => {
+  const testErrorMessage = 'Test';
+
+  showError(testErrorMessage);
+
+  const errorsEl = document.getElementById('errors');
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph.textContent).toBe(testErrorMessage);
 });
